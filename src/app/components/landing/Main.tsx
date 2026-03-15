1a1e41e1a6630513ed7d9f8689f125480af36f02
Joseph Kofi Leslie Lartey <lesliejoe2k2@gmail.com>
2026-03-15 16:32:55 +0000

    feat : brands section

--- src/app/components/landing/Main.tsx
+++ src/app/components/landing/Main.tsx
@@ -3,6 +3,9 @@ import {StaticImageData} from "next/image"
 import handBag from "../../../../public/assets/handBag.jpg"
 import watch from "../../../../public/assets/watch.jpg"
 import fashionBg from "../../../../public/assets/fashionWeek.jpg"
+import balenciaga from "../../../../public/assets/brand1.png"
+import ralphLauren from "../../../../public/assets/brand2.png"
+import dior from "../../../../public/assets/brand3.png"
 import { Heart } from "lucide-react"
 import Link from "next/link"
 
@@ -43,11 +46,37 @@ const trendingData: trendingData[] = [
     }
 ]
 
+type brandsData = {
+    idx: number,
+    image: StaticImageData,
+    description : string
+}
+
+const featuredBrands: brandsData[] = [
+    {   
+        idx: 1,
+        image: balenciaga,
+        description: "Shop Balenciaga"
+    },
+    {   
+        idx: 2,
+        image: ralphLauren,
+        description: "Shop Polo Ralph Lauren"
+    },
+    {   
+        idx: 3,
+        image: dior,
+        description: "Shop Dior"
+    }
+]
+
+
 function Main() {
     return (
-        <main className="max-w-7xl mx-auto">
+        <main className="">
             <Trending />
             <Fashweek />
+            <Brands />
         </main>
     )
 }
@@ -55,7 +84,7 @@ export default Main
 
 function Trending() {
     return (
-        <section className="max-w-7xl my-4 md:my-8 px-4 md:px-8">
+        <section className="max-w-7xl mx-auto my-4 md:my-8 px-4 md:px-8">
             <header className="flex justify-between items-center">
                 <h2 className="text-3xl text-[#000000] font-bold leading-[42px] font-fashion tracking-tight">Trending Now</h2>
                 <p className="text-[#000000] leading-[42px] font-nav border-b-2  text-[14px] font-medium border-b-black tracking-tighter">View All</p>
@@ -118,5 +147,30 @@ function Fashweek(){
                 </div>
             </div>
         </section>
+    )
+}
+
+function Brands() {
+    return(
+        <section className="bg-[#F9FAFB]">
+        <div className=" max-w-7xl mx-auto px-4 md:px-8 py-8 md:py-12 my-6 md:my-10">
+            <div className="flex flex-col md:flex-row justify-center items-center gap-6 md:gap-8">
+                {
+                    featuredBrands.map((brand, index) => (
+                        <div key={index} className="flex flex-col w-full max-w-[350px] bg-white pb-6 mx-auto">
+                            <div className="w-full h-full  lg:h-[331px] p-4 mb-4">
+                                <Image src={brand.image} alt={brand.description} className="w-full h-full object-cover" />
+                            </div>
+                            <div className="flex justify-center px-4">
+                                <Link href="" className="inline-block text-white bg-black text-[12px] md:text-[14px] lg:text-[16px] font-bold md:font-semibold font-lato py-2 px-10 transition-color text-center w-full max-w-[280px]">
+                                    {brand.description}
+                                </Link>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+         </section>
     )
 }
